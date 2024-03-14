99fc0f21ea9ab19a61e86cbc6bdad19c07752e1d
Shivam <singhkumarshivam15@gmail.com>
2024-03-15 02:50:49 +0530

    making some changes to create user

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,14 @@ const ErrorHandler = require("./middleware/error");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+    cors()
+  );
 app.use("/", express.static("uploads"));
 
 app.use(bodyParser.urlencoded({extended:true}));
