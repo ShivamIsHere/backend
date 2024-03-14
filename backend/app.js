124d46afcfdb7459f7b688e8cbd2eaae275c8cfd
Shivam <singhkumarshivam15@gmail.com>
2024-03-15 02:10:01 +0530

    created user controller for signup/register new user page

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const bodyParser = require("body-parser");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/", express.static("uploads"));
+
 app.use(bodyParser.urlencoded({extended:true}));
 //config
 if(process.env.NODE_ENV !== "PRODUCTION" ){
@@ -14,6 +16,11 @@ if(process.env.NODE_ENV !== "PRODUCTION" ){
         path:"backend/config/.env"
     })
 }
+
+// import routes
+const user = require("./controller/user");
+
+app.use("/api/v2/user", user);
 
 
 // it's for ErrorHandling
